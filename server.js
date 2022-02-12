c6c5c32c1b938669ef1111e158fc6be8a0c55bec
Karthik Belida <Karthik392001@gmail.com>
2022-02-12 22:34:39 +0530

    final

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ dotenv.config()
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
-}).then(console.log("DB CONNECTED"))
-.catch(err=>console.log(err))
+}).then()
+.catch(err=>{})
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
@@ -23,5 +23,5 @@ app.use(cors());
 app.use("/",uploadRouter)
 
 app.listen(port,()=>{
-    console.log("server listening on port %d in %s mode", this.address().port, app.settings.env)
+    
 })
